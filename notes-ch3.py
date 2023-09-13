d680a65e80e7c8834dfe27ad60eee9eecd72299d
lopazanski <cori.lopazanski@gmail.com>
2023-09-13 14:37:28 -0400

    update code for ch4

--- notes-ch3.py
+++ notes-ch3.py
@@ -1,93 +1,55 @@
-# Notes from Chapter 3
-# 3. Indexing, slicing, and subsetting dataframes in python
+# Notes for Chapter 3
+# 3. Starting with Data
 
-# Import pandas
+# Start by activating the environment
+# Go to terminal: source carpe/bin/activate
+
+# Import Libraries ------
 import pandas as pd
 
-# Read the survey data
-surveys_df = pd.read_csv('data/raw/surveys.csv')
-
-# Create a list of the survey species
-survey_species = surveys_df['species_id']
-
-# Select subsets
-surveys_df[0:3] # selects 0, 1, 2 (first 3 rows)
-surveys_df[:5] # selects first 5 rows
-surveys_df[-1:] # selects last element
-
-# Copying vs Referencing -----------------------------
-# Copy creates a complete new copy
-true_copy_surveys_df = surveys_df.copy()
-
-# Referencing creates a new variable referencing the same object
-# (e.g. no new copy will be made)
-ref_surveys_df = surveys_df
-
-# Reassigning values within a reference
-ref_surveys_df[0:3] = 0
-
-# Now the original data is going to be different
-true_copy_surveys_df.head()
-surveys_df.head()
-
-# Create a new clean dataframe from the original file ----
+# Read CSV using pandas ----
 surveys_df = pd.read_csv("data/raw/surveys.csv")
 
-# Slicing subsets of rows and columns
-# iloc: integer based indexing from 0
-surveys_df.iloc[0:3, 1:4] 
+surveys_df
 
-# loc: label based indexing by names
-surveys_df.loc[[0, 10], :] # select all columns for rows of index values 0 and 10
-surveys_df.loc[0, ['species_id', 'plot_id', 'weight']] # select 0th row and those three cols
-surveys_df.loc[[0, 10, 35549], :] # last row is index n-1
+# Exploring a dataframe & its attributes ----
+surveys_df.head() # head
+type(surveys_df) # type of the object
+surveys_df.dtypes # types for each of the columns
 
-# Can find a specific data element using dat.iloc[row, column]
-surveys_df.iloc[2, 6]
+surveys_df.columns # prints the column names
+surveys_df.shape # rows x columns
+surveys_df.head(15) # first 15 rows (0th through 14th)
+surveys_df.tail(15)
 
+# Getting the unique values
+pd.unique(surveys_df['species_id']) # unique values in species_id column
 
-# Range
-surveys_df[0:1] # just shows first row
-surveys_df[0] # error
-surveys_df[:4] # first 4 rows
-surveys_df[:-1] # everything except last row
+site_names = pd.unique(surveys_df['plot_id'])
+site_names.shape # 24 unique rows
+len(site_names) # 24
+surveys_df['plot_id'].nunique() # get the unique values without creating intermediate
 
-surveys_df.iloc[0:1] # just shows first row
-surveys_df.iloc[0] # first row as named list
-surveys_df.iloc[:4, :] # first 4 rows
-surveys_df.iloc[0:4, 1:4] # specified cols of first 4 rows
-surveys_df.loc[0:4, 1:4] # error: need to use labels
+# Groups in pandas -----
+# Get a bunch of summary stats for a given column
+surveys_df['weight'].describe() 
 
+# Extract one by one
+surveys_df['weight'].min()
+surveys_df['weight'].max()
+surveys_df['weight'].mean()
 
-# Subset data with criteria ---------
-surveys_df[surveys_df.year == 2002] # select all rows where year is 2002
-surveys_df[surveys_df.year != 2002] # select all rows where year is NOT 2002
-surveys_df[(surveys_df.year >= 1980) & (surveys_df.year <= 1985)] # between 1980-1985
-
-# Avaliable syntax
-# Equals: ==
-# Not equals: !=
-# Greater than, less than: > or <
-# Greater than or equal to >=
-# Less than or equal to <=
+# Can also summarize by variables
+grouped_data = surveys_df.groupby('sex')
+grouped_data.describe()
+grouped_data.mean(numeric_only = True)
 
 # Challenge:
-# Subset for Year == 1999 and Weight <= 8
-surveys_df[(surveys_df.year == 1999) & (surveys_df.weight <= 8)] # 5 rows
+grouped_data2 = surveys_df.groupby(['plot_id','sex'])
+grouped_data2.mean(numeric_only = True)
+surveys_df.groupby(['plot_id'])['weight'].describe()
 
-# Use mask to identify condition
-# Sex x to 5
-x = 5
+# Quickly creating summary counts in pandas
+# Count number of samples by species
+species_counts = surveys_df.groupby('species_id')['record_id'].count()
 
-# Now try:
-x > 5 # false
-x == 5 # true
-
-pd.isnull(surveys_df) # will show whether data is null
-
-# Select rows where there are null values
-surveys_df[pd.isnull(surveys_df).any(axis=1)]
-
-# 
-empty_weights = surveys_df[pd.isnull(surveys_df['weight'])]['weight']
-print(empty_weights)
